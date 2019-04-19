f98bbfb2f7982ba0264b4406503b4b2142b0f3fc
benamatare <ben.a.matare@gmail.com>
2019-04-19 18:34:53 -0500

    changed loading to functional component

--- frontend/twitch_ui/src/pages/loading/components/loading.js
+++ frontend/twitch_ui/src/pages/loading/components/loading.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import '../css/loading.css'
+import React, { Component } from "react";
+import "../css/loading.css";
 
-const Spinner = require('react-spinkit');
+const Spinner = require("react-spinkit");
 
-export default class Loading extends Component {
-  render() {
-    return (
-      <div className='load'>
-          <Spinner name='wave' />
-      </div>
-    )
-  }
-}
+const Loading = () => {
+  return (
+    <div className="load">
+      <Spinner name="wave" />
+    </div>
+  );
+};
+
+export default Loading;
